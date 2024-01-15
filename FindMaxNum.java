bc947a4fe933d71a3dcd7543c3bc05fe243adbcd
denver123-creator <59923346+denver123-creator@users.noreply.github.com>
2024-01-15 01:34:38 -0800

    Update FindMaxNum.java

--- FindMaxNum.java
+++ FindMaxNum.java
@@ -1,19 +1,25 @@
-public class FindMaxNum { 
- public static void main(String[] args) {
-        List<Integer> nums = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter three numbers:");
-        for(int i = 0; i < 3; i++){
-            int x = scanner.nextInt();
-            nums.add(x);
-        }
-        int largest = nums.get(0);
-        for (int num : nums){
-            if (largest < num) {
-                largest = num;
-            }
-        }
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-        System.out.println("The largest number is: " + largest);
- }
-}
+public class FindMaxNum { 
+    public static void main(String[] args) {
+           List<Integer> nums = new ArrayList<>();
+           Scanner scanner = new Scanner(System.in);
+           System.out.println("Enter three numbers:");
+           for(int i = 0; i < 3; i++){
+               int x = scanner.nextInt();
+               nums.add(x);
+           }
+           int largest = nums.get(0);
+           for (int num : nums){
+               if (largest < num) {
+                   largest = num;
+               }
+           }
+   
+           System.out.println("The largest number is: " + largest);
+           scanner.close();
+    }
+   }
+   
